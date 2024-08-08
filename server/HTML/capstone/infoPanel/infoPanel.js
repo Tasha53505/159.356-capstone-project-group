bb6c71908299b1d0cc9e488502548fab6c2cd5cc
Konrad <konradkiefersauer@gmail.com>
2024-08-08 18:08:10 +1200

    Added Artist Page and option to change between song and artist page

--- server/HTML/capstone/infoPanel/infoPanel.js
+++ server/HTML/capstone/infoPanel/infoPanel.js
@@ -1,7 +1,26 @@
 var infoPanelBtn = document.querySelector('.infoPanelBtn');
 var infoPanel = document.querySelector('.infoPanel');
+var artistPanel = document.querySelector('.infoArtistPanel');
+var changeButton = document.querySelector('.changePanelBtn');
+
+changeButton.addEventListener('click', function () {
+    if ((artistPanel.classList.contains('active')) || (infoPanel.classList.contains('active'))) {
+        artistPanel.classList.toggle('active');
+        infoPanel.classList.toggle('active');
+        if (changeButton.innerHTML == 'B') {
+            changeButton.innerHTML = 'A';
+        } else {
+            changeButton.innerHTML = 'B';
+        }
+    }
+});
 
 infoPanelBtn.addEventListener('click', function () {
-    infoPanel.classList.toggle('hidden');
-    infoPanelBtn.classList.toggle('hidden');
+    if (!(infoPanelBtn.classList.contains('hidden')) && (changeButton.innerHTML == 'A')) {
+        infoPanel.classList.toggle('active');
+    } else if (!(infoPanelBtn.classList.contains('hidden')) && (changeButton.innerHTML == 'B')) {
+        artistPanel.classList.toggle('active');
+    } else {
+        //do nothing
+    }
 });
