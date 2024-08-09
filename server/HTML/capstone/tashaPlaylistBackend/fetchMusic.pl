1e4474aa5454940a3470787612e971d304a9ca2a
Tasha Carr <ncarr63@gmail.com>
2024-08-09 13:34:44 +1200

    Basic implementation of fetching Music

--- server/HTML/capstone/tashaPlaylistBackend/fetchMusic.pl
+++ server/HTML/capstone/tashaPlaylistBackend/fetchMusic.pl
@@ -2,4 +2,12 @@ use strict
 use warnings
 use JSON 
 
-my $musicDirectory = '/'
+my $musicDirectory = 'capstone\tashaPlaylistBackend\testMusic'; 
+opendir(my $dh, $musicDirectory) || die "cannot open file directory: $!" # $dh reads musicDirectorys content. If not found, Die gives an error. --> $! means a specifc error message
+
+my @files;
+while(readdir $dh) { #loops over all content in directory
+    next unless /\.(mp3|wav)$/; #Regex pattern for file types --> Skips to next file if mp3, wav doesn't match
+    push @files, $_; #Any files with those endings are found, pushes it to the array.
+}
+closedir $dh
