ec570ab693b3eb77a9b07107418a1a7a7815a5b8
Tasha Carr <ncarr63@gmail.com>
2024-08-15 17:19:42 +1200

    Moved Back Button

--- server/HTML/capstone/playlistHome.js
+++ server/HTML/capstone/playlistHome.js
@@ -150,8 +150,9 @@ document.getElementById('settingsButton').addEventListener('click', function() {
     // Add HTML for the settings content with tabs and advanced toggle
     settingsContainer.innerHTML = `
         <div class="settingsHeader">
+                    <button class="backButtonSettings">
+
             <h2>Settings</h2>
-            <button class="backButtonSettings">
                 <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 1024 1024">
                     <path fill="#ffffff" d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64"/>
                     <path fill="#ffffff" d="m237.248 512l265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312z"/>
