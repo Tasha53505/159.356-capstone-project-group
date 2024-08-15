ab3c67c9ba3cdd8c2f7c5145ecb2df4ab4a85122
Tasha Carr <ncarr63@gmail.com>
2024-08-15 17:22:15 +1200

    adjusting CSS for settings

--- server/HTML/capstone/playlistHome.js
+++ server/HTML/capstone/playlistHome.js
@@ -149,9 +149,8 @@ document.getElementById('settingsButton').addEventListener('click', function() {
     
     // Add HTML for the settings content with tabs and advanced toggle
     settingsContainer.innerHTML = `
+        <button class="backButtonSettings">
         <div class="settingsHeader">
-                    <button class="backButtonSettings">
-
             <h2>Settings</h2>
                 <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 1024 1024">
                     <path fill="#ffffff" d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64"/>
