efcdfebedee073e8e0bfdb4434c080a88898f774
Tasha Carr <ncarr63@gmail.com>
2024-08-15 17:18:44 +1200

    adjusting CSS for settings

--- server/HTML/capstone/playlistHome.js
+++ server/HTML/capstone/playlistHome.js
@@ -151,7 +151,7 @@ document.getElementById('settingsButton').addEventListener('click', function() {
     settingsContainer.innerHTML = `
         <div class="settingsHeader">
             <h2>Settings</h2>
-            <button class="backButton">
+            <button class="backButtonSettings">
                 <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 1024 1024">
                     <path fill="#ffffff" d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64"/>
                     <path fill="#ffffff" d="m237.248 512l265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312z"/>
@@ -185,7 +185,7 @@ document.getElementById('settingsButton').addEventListener('click', function() {
     document.body.appendChild(settingsContainer);
 
     // Add event listener for the back button
-    settingsContainer.querySelector('.backButton').addEventListener('click', function() {
+    settingsContainer.querySelector('.backButtonSettings').addEventListener('click', function() {
         settingsContainer.classList.remove('showSettings');
         setTimeout(function() {
             settingsContainer.remove();
