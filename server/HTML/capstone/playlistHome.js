ca40d5312e8e341a39a7b532eb13f00b5f82cd34
Tasha Carr <ncarr63@gmail.com>
2024-08-17 15:46:16 +1200

    Updated path with an extra /

--- server/HTML/capstone/playlistHome.js
+++ server/HTML/capstone/playlistHome.js
@@ -130,7 +130,7 @@ document.getElementById('allSongs').addEventListener('click', function() {
     }, 10);
 
     // Fetch music and display in the newSongsContainer
-    fetch('server/HTML/capstone/tashaPlaylistBackend/fetchMusic.pl')
+    fetch('/HTML/capstone/tashaPlaylistBackend/fetchMusic.pl')
     .then(response => {
         if (!response.ok) {
             throw new Error(`HTTP error.  Status is: ${response.status}`);
