53346b907e3f362c66e2f539a718369b9cdfeaca
Tasha Carr <ncarr63@gmail.com>
2024-08-17 16:27:30 +1200

    Updated path

--- server/HTML/capstone/playlistHome.js
+++ server/HTML/capstone/playlistHome.js
@@ -117,7 +117,7 @@ document.getElementById('allSongs').addEventListener('click', function() {
     newAllSongsContainer.innerHTML = 
         `<h3>Songs</h3>
         <button class="backButton"><svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 1024 1024"><path fill="#ffffff" d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64"/><path fill="#ffffff" d="m237.248 512l265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312z"/></svg>
-            </button>
+        </button>
 
 
         <div id="musicList"></div>`; // Container to display the music list from FetchMusic.pl
