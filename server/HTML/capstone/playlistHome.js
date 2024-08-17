f1605f2134168ae575ccdc6be50478f2855c3cf4
Tasha Carr <ncarr63@gmail.com>
2024-08-17 15:59:55 +1200

    test html

--- server/HTML/capstone/playlistHome.js
+++ server/HTML/capstone/playlistHome.js
@@ -130,7 +130,7 @@ document.getElementById('allSongs').addEventListener('click', function() {
     }, 10);
 
     // Fetch music and display in the newSongsContainer
-    fetch('tashaPlaylistBackend\\fetchMusic.pl')
+    fetch('tashaPlaylistBackend\\test.html')
     .then(response => {
         if (!response.ok) {
             throw new Error(`HTTP error.  Status is: ${response.status}`);
