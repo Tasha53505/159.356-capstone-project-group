2b562edd8d4897e157c5cf6d847af5a0d25d6d5b
Tasha Carr <ncarr63@gmail.com>
2024-08-17 15:44:05 +1200

    Updated path

--- server/HTML/capstone/tashaPlaylistBackend/fetchMusic.pl
+++ server/HTML/capstone/tashaPlaylistBackend/fetchMusic.pl
@@ -1,43 +1,24 @@
+
 #!/usr/bin/perl
 use strict;
 use warnings;
 use JSON;
 
-# Set up the music directory
-my $musicDirectory = 'C:\Program Files\Squeezebox\server\HTML\capstone\tashaPlaylistBackend\testMusic'; # Note: Forward slashes or double backslashes are required for Windows paths.
-opendir(my $dh, $musicDirectory) || die "Cannot open file directory: $!";
+my $musicDirectory = 'C:\Program Files\Songs'; # Need to find better way to get directory, different for others (potentially use FindBin).
+opendir(my $dh, $musicDirectory) || die "cannot open file directory: $!"; # $dh reads musicDirectorys content. If not found, Die gives an error. --> $! means a specifc error message
 
-# Read and filter the music files
 my @files;
-while (readdir $dh) {
-    next unless /\.(m4a|aif|dff|flac|ogg|wav)$/; # Regex pattern for file types
-    push @files, $_;
+while(readdir $dh) { #loops over all content in directory
+    next unless /\.(m4a|aif|dff|flac|ogg|wav)$/; #Regex pattern for file types --> Skips to next file if mp3, wav doesn't match
+    push @files, $_; #Any files with those endings are found, pushes it to the array.
 }
 closedir $dh;
 
-# Generate HTML output manually
-my $html_output = '';
-foreach my $file (@files) {
-    my $title = $file;
-    my $item = $file;
-
-    $html_output .= qq{
-        <div id="$title">
-            <span class="songInfoTitle">
-                $title: 
-            </span>
-            <span class="songInfoText">$item</span>
-        </div>
-    };
-}
-
-# Prepare the JSON response
+#Adding debugging statements for console log
 my %response = (
     files => \@files,
-    log => "Music has been fetched. No Errors",
-    html_output => $html_output
+    log => "Music has been fetched. No Errors"
 );
 
-# Print the JSON response
 print "Content-Type: application/json\n\n";
 print encode_json(\%response);
