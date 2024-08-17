d145e72837d4bb8f9cae503b0741d8b2b42e4789
Tasha Carr <ncarr63@gmail.com>
2024-08-17 16:24:44 +1200

    Updated path

--- server/HTML/capstone/playlistHome.js
+++ server/HTML/capstone/playlistHome.js
@@ -130,7 +130,7 @@ document.getElementById('allSongs').addEventListener('click', function() {
     }, 10);
 
     // Fetch music and display in the newSongsContainer
-    fetch('https://www.google.com/')
+    fetch('C:/Program Files/Squeezebox/server/HTML/capstone')
     .then(response => {
         if (!response.ok) {
             throw new Error(`HTTP error.  Status is: ${response.status}`);
