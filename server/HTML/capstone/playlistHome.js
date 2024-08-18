6a77ed0c1277ec367cdcc59f2cca5ec338dcf863
Tasha Carr <ncarr63@gmail.com>
2024-08-18 16:03:34 +1200

    changing file paths

--- server/HTML/capstone/playlistHome.js
+++ server/HTML/capstone/playlistHome.js
@@ -130,7 +130,7 @@ document.getElementById('allSongs').addEventListener('click', function() {
     }, 10);
 
     // Fetch music and display in the newSongsContainer
-    fetch('/fetch.pl')
+    fetch('tashaPlaylistBackend/testMusic')
     .then(response => {
         if (!response.ok) {
             throw new Error(`HTTP error.  Status is: ${response.status}`);
