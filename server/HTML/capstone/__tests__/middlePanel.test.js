184f97f7441b74951d2d161b8dcea724fa508636
Ben <101218917+yesnice@users.noreply.github.com>
2024-10-11 12:20:03 +1300

    test for togglePause

--- server/HTML/capstone/__tests__/middlePanel.test.js
+++ server/HTML/capstone/__tests__/middlePanel.test.js
@@ -11,62 +11,24 @@ global.SqueezeJS = {
     }
 };
 
-document.body.innerHTML = `
-   <div class="mainContainer">
-        <div class="albumContainer">
-            <div class="album">
-                <div id = "coverart"></div>
-            </div>
-        </div>
-        <div class="textContainer">
-            <h1 id ="title"></h1>
-            <h2 id ="artist"></h2>
-            <h2 id ="bitrate"></h2>
-        </div>
-        <div class="playerContainer">
-            <div class="player">
-                <div id="playtimePanel">
-                    <div id="ctrlPlaytime"></div>
-                    <div id="playbar">
-                        <div id="ctrlProgress"></div>
-                    </div>
-                    <div id="ctrlRemainingTime"></div>
-                </div>
-                <!--<div id="ctrlProgress"></div>
-                <div class="timeInfo">
-                    <p class="playtime" id="playtime"></p>
-                    <p class="maxtime" id="maxtime"></p>
-                </div>-->
-                <div class="buttons">
-                    <div class="control">
-                        <div id="repeatButton" class="repeatButtonInactive">   
-                        </div>
-                    </div>
-                    <div class="control">
-                        <div id="prevButton" class="prevButton">   
-                        </div>
-                    </div>
-                    <div class="control">
-                        <div id="playPause" class="">   
-                        </div>
-                    </div>
-                    <div class="control">
-                        <div id="nextButton" class="nextButton">   
-                        </div>
-                    </div>
-                    <div class="control">
-                        <div id="shuffleButton" class="shuffleButtonInactive">   
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-`
+document.body.innerHTML = html;
 
 describe('Button functionality', () => {
-    test('togglePause when play/pause is clicked', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test('see if the togglePause function works', () => {
+        // Button exists
         const playPauseButton = document.getElementById('playPause');
+        expect(playPauseButton).toBeInTheDocument();
+
+        // Mock event listener
+        playPauseButton.addEventListener('click', () => {
+            SqueezeJS.Controller.togglePause();
+        });
+
+        // Checks if it works
         fireEvent.click(playPauseButton);
         expect(SqueezeJS.Controller.togglePause).toHaveBeenCalled();
     });
