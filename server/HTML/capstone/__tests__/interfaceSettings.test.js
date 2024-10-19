a4350fdc043e0823c04220be81be5781656755de
Ben <101218917+yesnice@users.noreply.github.com>
2024-10-20 08:25:55 +1300

    added code back to test

--- server/HTML/capstone/__tests__/interfaceSettings.test.js
+++ server/HTML/capstone/__tests__/interfaceSettings.test.js
@@ -23,6 +23,37 @@ describe('Interface Settings', () => {
 
     // See if drop down menu exists and includes value 3
     test('title format dropdown menu exists and has an option of value 3 and text "TRACKNUM. ARTIST - TITLE"', () => {
+        document.body.innerHTML +=`
+        <div class="tooltipDrowndown">     
+            <div class="tooltipContainer">
+                <div class="whatIsThis" style="display: flex; align-items: center; gap: 0.5em;">
+                    <h4 style="display: inline-block; vertical-align: middle;">Title Format</h4>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24" class="help-icon" style="display: inline-block; vertical-align: middle;">
+                        <path fill="currentColor" d="M11.95 18q.525 0 .888-.363t.362-.887t-.362-.888t-.888-.362t-.887.363t-.363.887t.363.888t.887.362m-.9-3.85h1.85q0-.825.188-1.3t1.062-1.3q.65-.65 1.025-1.238T15.55 8.9q0-1.4-1.025-2.15T12.1 6q-1.425 0-2.312.75T8.55 8.55l1.65.65q.125-.45.563-.975T12.1 7.7q.8 0 1.2.438t.4.962q0 .5-.3.938t-.75.812q-1.1.975-1.35 1.475t-.25 1.825M12 22q-2.075 0-3.9-.787t-3.175-2.138T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22" />
+                    </svg>  
+                </div>
+                    <div class="tooltip">  
+                        These are the formats available for selection both in the web interface and on players. 
+                    </div>
+             </div>
+            <div class="prefs">
+                 <form id="titleFormatWebForm" class="">
+                    <select class="stdedit" name="pref_titleFormatWeb" id="titleFormatWeb">
+                        <option value="0">TITLE</option>
+                        <option value="1">DISC-TRACKNUM. TITLE</option>
+                        <option value="2">TRACKNUM. TITLE</option>
+                        <option value="3">TRACKNUM. ARTIST - TITLE</option>
+                        <option value="4">TRACKNUM. TITLE (ARTIST)</option>
+                        <option value="5">TRACKNUM. TITLE - ARTIST - ALBUM</option>
+                        <option value="6">TRACKNUM. TITLE from ALBUM by ARTIST</option>
+                        <option value="7">TITLE (ARTIST)</option>
+                        <option value="8">ARTIST - TITLE</option>
+                    </select><br><br>
+                    <input name="pref_update_titleFormatWeb" type="button" class="stdclick rescanButton disableOnScan saveSettings" id="titleFormatWebUpdateButton" onclick="titleFormatWebForm(this.form); return false;"  value="Save Title Format"><br><br>
+                </form>
+            </div>
+        </div>  
+        `
         const titleFormatMenu = document.getElementById('titleFormatWeb');
         expect(titleFormatMenu).toBeInTheDocument();
         const options = Array.from(titleFormatMenu.options);
